f6e6330e6b61f6f961f1d0f809c83cb9eb461d25
BuildTools <unconfigured@null.spigotmc.org>
2022-09-23 05:42:30 -0400

    Fixed background-attachment issue causing lag

--- js/about.js
+++ js/about.js
@@ -1,3 +1,13 @@
 $(".experienceHeader").click(function () {
 	$(this).parent().toggleClass("expanded");
 });
+
+$(".educationCard").click(function () {
+	if ($(this).hasClass("opening")) {
+		$(this).removeClass("opening");
+		$(this).addClass("closing");
+	} else {
+		$(this).addClass("opening");
+		$(this).removeClass("closing");
+	}
+});
